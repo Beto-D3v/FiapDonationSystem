f7cab116a2e20c31612a2f88056ddbbfc3e769e6
Roberto Macedo <robertomacedo3007@gmail.com>
2023-06-20 06:50:44 -0300

    Criação do projeto

---
+++ FiapDonationSystem/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace FiapDonationSystem
+{
+    public static class Program
+    {
+        public static void Main (string[] args)
+        {
+            Console.WriteLine("FIAP DONATION SYSTEM");
+
+            Console.WriteLine("Preencha os dados cadastrais abaixo: ");
+
+            Console.WriteLine("Digite o nome: ");
+            string nome = Console.ReadLine();
+
+            Console.WriteLine("Digite o seu telefone: \n");
+            string telefone = Console.ReadLine();
+
+            Console.WriteLine("Digite o seu e-mail: \n");
+            string email = Console.ReadLine();
+
+            Console.WriteLine("Digite a sua senha: \n");
+            string senha = Console.ReadLine();
+
+            Console.WriteLine("Digite o seu apelido: \n");
+            string apelido = Console.ReadLine();
+        }
+    }
+}
